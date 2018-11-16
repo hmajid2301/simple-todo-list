79e0f741c7c025307aecfceadcd472130e10f481
Haseeb Majid <hmajid2301@gmail.com>
2018-11-16 19:46:40 +0000

    :fire: Removed redundant files
    Removed soms redundant files, no longer implementing this feature.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, TOGGLE_TODO } from './actionTypes';
+import { ADD_TODO } from './actionTypes';
 
 let nextId = 0;
 
@@ -6,9 +6,4 @@ export const addTodo = (text) => ({
   type: ADD_TODO,
   id: nextId++,
   text,
-});
-
-export const toggleTodo = (id) => ({
-  type: TOGGLE_TODO,
-  id: id
 });
